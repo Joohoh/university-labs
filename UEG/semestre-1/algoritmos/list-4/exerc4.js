0ca855a6c373b64970326bc35bbed2ba51219cb8
Jordana M. B. dos Santos <martins.benunes1@gmail.com>
2026-03-12 19:09:12 -0300

    Update exerc4.js

--- UEG/semestre-1/algoritmos/list-4/exerc4.js
+++ UEG/semestre-1/algoritmos/list-4/exerc4.js
@@ -3,17 +3,14 @@ Efetuar a troca dos valores de forma que A passe a
 armazenar o valor de B e que B passe armazenar o valor de
 A. Imprimir os valores trocados. */
 
+function trocarVariaveis(a, b){
+    return [b, a];
+}
+
 const prompt = require("prompt-sync")();
+let a = parseInt(prompt("Digite um valor inteiro para A: "));
+let b = parseInt(prompt("Digite um valor inteiro para B: "));
 
-let A = Number(prompt("Digite um valor para A: "));
-let B = Number(prompt("Digite um valor para B: "));
+[a, b] = trocarVariaveis(a, b);
 
-//Criar uma variável temporaria para poder fazer essa conversão
-let temp = A;
-A = B;
-B = temp;
-
-// Jeito moderno de fazer
-// [A, B] =  [B, A];
-
-console.log(`O valor de 'A' é: ${A} e o valor de 'B' é: ${B}`);
+console.log(`O novo valor de A é: ${a} e de B é ${b}`);
