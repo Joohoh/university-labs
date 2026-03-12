7a27f0835466907a9b799d0831e0187ac076e443
Jordana M. B. dos Santos <martins.benunes1@gmail.com>
2026-03-12 19:07:49 -0300

    Update exerc3.js

--- UEG/semestre-1/algoritmos/list-3/exerc3.js
+++ UEG/semestre-1/algoritmos/list-3/exerc3.js
@@ -7,13 +7,17 @@ valoracumulado=((1+i^n - 1))/i * P
 
 onde i = taxa, P = aplicação mensal e n = número de meses. */
 
+function calcularRendimento(valorAplicacaoMensal, taxa, numeroMeses){
+    const taxaDecimal = taxa / 100;
+    const rendimento = (((1 + taxaDecimal) ** numeroMeses - 1) / taxaDecimal) * valorAplicacaoMensal;
+    return rendimento;
+}
+
 const prompt = require("prompt-sync")();
+const valorAplic = Number(prompt("Digite o valor que foi aplicado: R$ "));
+const tx = Number(prompt("Digite o valor da taxa: "));
+const numMeses = Number(prompt("Digite a quantidade de meses: "));
 
-const aplicacaoMensal = Number(prompt("Digite o valor da sua aplicação mensal: "));
-const numeroDeMeses = Number(prompt("Digite a quantidade de meses de aplicação: "));
-const taxa = Number(prompt("Digite o valor da taxa mensal: "));
+const rendimentoTotal = calcularRendimento(valorAplic, tx, numMeses);
 
-const valorAcumulado = (((1 + taxa)** numeroDeMeses - 1) / taxa) * aplicacaoMensal; 
-
-console.log(`Seu rendimento foi de: R$ ${valorAcumulado.toFixed(2)}`);
-
+console.log(`O valor do seu rendimento final é de: R$ ${rendimentoTotal.toFixed(2)}`);
