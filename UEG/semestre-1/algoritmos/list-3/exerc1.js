d57ce4645dfe8ce42ba99d74189ebd4d7e337717
Jordana M. B. dos Santos <martins.benunes1@gmail.com>
2026-03-12 19:07:25 -0300

    Update exerc1.js

--- UEG/semestre-1/algoritmos/list-3/exerc1.js
+++ UEG/semestre-1/algoritmos/list-3/exerc1.js
@@ -3,13 +3,17 @@ B(x2,y2), que serão informadas via teclado, desenvolver um
 programa que calcule a distância entre A e B.
 distancia = raiz quadrada( (x2 - x1)^2 + (y2 - y1)^2) ) */
 
+function calcularPontos(x1, x2, y1, y2){
+    const pontos = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    return pontos;
+}
+
 const prompt = require("prompt-sync")();
+const a1 = Number(prompt("Digite o ponto A1: "));
+const a2 = Number(prompt("Digite o ponto A2: "));
+const b1 = Number(prompt("Digite o ponto B1: "));
+const b2 = Number(prompt("Digite o ponto B2: "));
 
-const pontoAX = Number(prompt("Digite o valor de x do ponto A: "));
-const pontoAY = Number(prompt("Digite o valor de y do ponto A: "));
-const pontoBX = Number(prompt("Digite o valor de x do ponto B: "));
-const pontoBY = Number(prompt("Digite o valor de y do ponto B: "));
+const distancia = calcularPontos(a1, a2, b1, b2);
 
-const distancia = Math.sqrt((pontoAX - pontoBX) ** 2 + (pontoAY - pontoBY) ** 2);
-
-console.log(`A distancia entre esses pontos é: ${distancia.toFixed(2)}`);
+console.log(`A distancia entre os pontos é de: ${distancia.toFixed(2)}`);
